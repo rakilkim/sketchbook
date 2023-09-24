39999e2a974bfdc0ce784705f42fe9d5a99136ca
zxcvbbbnm <123872988+zxcvbbbnm@users.noreply.github.com>
2023-09-23 21:53:58 -0700

    Basic sketchbook done, user able to change the dimensions

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
-const container = document.querySelector('.container');
+let dimension = 16;
+let new_dimension = "";
+const BOARD_SIDE = 640;
+let r = 0;
+let g = 0;
+let b = 0;
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (let i = 0; i < 16; i++) {
-        const grid = document.createElement('div');
-        grid.textContent = '.'
-        grid.classList.add('grid');
-        row.appendChild(grid);
+let container = document.querySelector('#container');
+
+function createBoard(dimension) {
+    size = BOARD_SIDE/dimension
+    dimension = parseInt(dimension);
+    for (let i = 0; i < dimension; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let i = 0; i < dimension; i++) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            grid.style.width = size + 'px';
+            grid.style.height = size + 'px';
+            row.appendChild(grid);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
+createBoard(dimension);
+reset();
+
+function reset() {
+    const grids = document.querySelectorAll(".grid");
+    let coloring = false;
+    grids.forEach((grid) => {
+        grid.addEventListener('mousedown', (e) => {
+            coloring = true;
+            grid.style.background = "rgb("+r+","+g+","+b+")";
+            e.preventDefault();
+        });
+        grid.addEventListener('mousemove', () => {
+            if (coloring) {
+                grid.style.background = "rgb("+r+","+g+","+b+")";
+            }
+        });
+        grid.addEventListener('mouseup', () => {
+            coloring = false;
+        });
+    });
+}
+
+
+const dimensions = document.getElementById("dimensions");
+dimensions.addEventListener('keyup', function(e)  {
+    console.log(e)
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        document.getElementById("generate").click();
+    }
+    else if (isFinite(e.key)) {
+        new_dimension += e.key;
+    }
+});
+
+const generate = document.getElementById("generate");
+generate.addEventListener('click', () => {
+    if (parseInt(new_dimension) > 1 && parseInt(new_dimension) < 101) {
+        document.getElementById('container').innerHTML = "";
+        createBoard(new_dimension);
+        reset();
+    }
+    dimensions.value = "";
+    new_dimension = "";
+});
